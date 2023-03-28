76c7c69f6dd812b84f227350d9b933d08cda2a4d
S <q20142015@yandex.ru>
2023-03-28 09:39:50 +0300

    Вы доставляете гуманитарную помощь в виде ящиков одинакового размера. У вас есть грузовики и контейнеры. В каждый грузовик влезает максимум 12 контейнеров. В каждый контейнер ­– не более 27 ящиков. Ящики, контейнеры и грузовики пронумерованы.
    Нужно написать программу, которая будет распределять ящики по контейнерам и грузовикам в зависимости от их количества, а также напечатает, сколько всего нужно грузовиков и контейнеров. На вход программы через консоль должно подаваться число ящиков.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    private static final int maxBoxInCont = 27;
+    private static final int maxContInTrucks = 12;
+
+    public static void main(String[] args) {
+        System.out.println("Skolko yashikov?");
+        var scaner = new Scanner(System.in);
+        int boxCount = Integer.parseInt(scaner.nextLine());
+        trucksAndContainers(boxCount);
+    }
+
+    public static void trucksAndContainers(int box) {
+
+        int cont = box / maxBoxInCont + 1;
+        int trucks = cont / maxContInTrucks + 1;
+
+        System.out.println("gruzovikov nuzno:" + trucks);
+        System.out.println("konteinerov nuzno:" + cont);
+        System.out.println();
+
+        for (int i = 1; i <= trucks; i++) {
+            System.out.println("gruzovik:" + i);
+            int ogrC = (i < trucks) ? maxContInTrucks * i : cont;
+            for (int j = 1 + maxContInTrucks * (i - 1); j <= ogrC; j++) {
+                System.out.println(" conteiner:" + j);
+                int ogrY = (j < cont) ? maxBoxInCont * j : box;
+                for (int k = 1 + maxBoxInCont * (j - 1); k <= ogrY; k++) {
+                    System.out.println("  yashik:" + k);
+                }
+            }
+        }
+
+    }
 }
