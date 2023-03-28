1c5f6a2121c98f7a443499003a0d7ddcacd46595
S <q20142015@yandex.ru>
2023-03-28 14:41:11 +0300

    Выполняйте задание в репозитории “java_NumbersAndDates” в классе src/main/java/practice/Birthdays.java. Реализуйте метод: public static String collectBirthdays(int year, int month, int day),
    который вернёт строки, содержащие все ваши прошедшие дни рождения от момента рождения до текущей даты в требуемом формате. Дата рождения передаётся в метод тремя числами.
    
    Формат требуемого вывода, если ваш день рождения — 31 декабря 1990 года, а сейчас — 1 апреля 2022 года:

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class Main {
@@ -5,10 +13,16 @@ public class Main {
     private static final int maxContInTrucks = 12;
 
     public static void main(String[] args) {
+        String str = collectBirthdays(1990, 12, 02);
+        System.out.println(str);
         System.out.println("Skolko yashikov?");
         var scaner = new Scanner(System.in);
         int boxCount = Integer.parseInt(scaner.nextLine());
+        long te = System.currentTimeMillis();
         trucksAndContainers(boxCount);
+        long tn = System.currentTimeMillis();
+        long zatrtime = tn - te;
+        System.out.println(zatrtime);
     }
 
     public static void trucksAndContainers(int box) {
@@ -32,5 +46,22 @@ public class Main {
             }
         }
 
+    }
+
+    public static String collectBirthdays(int year, int month, int day) {
+        String rString = "";
+        LocalDate nowDay = LocalDate.now();
+        LocalDate birthDay = LocalDate.of(year, month, day);
+
+        int i=0;
+        do {
+            rString += i+"-";
+            rString += birthDay.format(DateTimeFormatter.ofPattern("dd.MMMyyyy"));
+            rString += "-" + birthDay.getDayOfWeek();
+            birthDay = birthDay.plusYears(1);
+            rString += "\n";
+            i=i+1;
+        } while (birthDay.isBefore(nowDay));
+        return rString;
     }
 }
